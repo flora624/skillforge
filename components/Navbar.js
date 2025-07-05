2e7c74106fbcfa03b77939c1fd2ca470d5f05c34
SkillForge <saranshsharma595@gmail.com>
2025-07-05 20:33:32 +0530

    changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -17,8 +17,6 @@ export default function Navbar() {
   const handleLogout = async () => {
     try {
       await signOut(auth);
-      // After logout, you might want to redirect the user to the homepage
-      // This requires the useRouter hook, but for now, it just logs out.
     } catch (error) {
       console.error("Error signing out: ", error);
     }
@@ -37,11 +35,28 @@ export default function Navbar() {
           <li><Link href="/#features">Why SkillForge?</Link></li>
           <li><Link href="/#projects">Projects</Link></li>
           
-          {/* This logic now shows a direct link to the profile page */}
           {isClient && !loading && (
             isLoggedIn ? (
               <>
-                <li><Link href="/profile" className="profile-icon-link"><i className="fas fa-user-circle"></i></Link></li>
+                {/* --- THIS IS THE NEW SVG ICON LINK --- */}
+                <li>
+                  <Link href="/profile" className="profile-icon-link">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="profile-svg-icon"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </Link>
+                </li>
+                {/* --- END OF SVG ICON LINK --- */}
+                
                 <li><a onClick={handleLogout} className="logout-button">Logout</a></li>
               </>
             ) : (
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,15 @@
-import { AuthProvider } from '../context/AuthContext'; // Make sure this path is correct
+import { AuthProvider } from '../context/AuthContext';
 import '../styles/globals.css';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    // The AuthProvider MUST wrap everything else.
     <AuthProvider>
       <Head>
         <title>SkillForge - Build Your Career Portfolio</title>
         <meta name="description" content="Tackle real-world problems from every career domain. Build a portfolio that gets you hired." />
-        {/* Using the async script tag is better for performance */}
-        <script src="https://kit.fontawesome.com/3a4339b972.js" crossOrigin="anonymous" async></script>
       </Head>
+      {/* The failing Font Awesome script has been REMOVED */}
       <Component {...pageProps} />
     </AuthProvider>
   );
