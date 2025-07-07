904b0fb5549d02f33130e2b0f6271f6c81424a00
SkillForge <saranshsharma595@gmail.com>
2025-07-07 11:31:58 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Head>
+        <meta name="google-adsense-account" content="ca-pub-6379719724029640"></meta>
         <title>SkillForge - Build Your Career Portfolio</title>
         <meta name="description" content="Tackle real-world problems from every career domain. Build a portfolio that gets you hired." />
       </Head>
