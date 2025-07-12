05a77b651d56ec447eb23ac1476bcff9b188dca1
SkillForge <saranshsharma595@gmail.com>
2025-07-12 23:25:45 +0530

    s

--- components/Navbar.js
+++ components/Navbar.js
@@ -86,12 +86,12 @@ export default function Navbar() {
                     </Link>
                   </li>
                   {/* --- END OF SVG ICON LINK --- */}
-                  <li><a onClick={handleLogout} className="btn btn-text logout-button navbar-action-btn">Logout</a></li>
+                  <li><a onClick={handleLogout} className="btn btn-text logout-button">Logout</a></li>
                 </>
               ) : (
                 <>
-                <li><Link href="/login" className="btn btn-text logout-button navbar-action-btn">Login</Link></li>
-                <li><Link href="/signup" className="btn btn-text logout-button navbar-action-btn">Signup</Link></li>
+                <li><Link href="/login" className="btn btn-text logout-button">Login</Link></li>
+                <li><Link href="/signup" className="btn btn-text logout-button">Signup</Link></li>
                 </>
               )
             )}
