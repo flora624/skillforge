4e2e6689f4f022bc1f3cbdf4a3838407c9e64371
SkillForge <saranshsharma595@gmail.com>
2025-07-12 22:33:09 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -45,62 +45,61 @@ export default function Navbar() {
     }
   };
 
- return (
-  <>
-    <nav className={`navbar${scrolled ? ' scrolled' : ''}${navbarHidden ? ' navbar--hidden' : ''}`}>
-      <div className="container">
-        {/* The Logo */}
-        <Link href="/" className="logo">
-          <div style={{ width: '80px', height: '80px', position: 'relative' }}>
-            <Image
-              src="/logo.png"
-              alt="SkillFroge Logo"
-              fill
-              priority
-              style={{ objectFit: 'contain' }}
-            />
-          </div>
-        </Link>
-        {/* The Main Navigation Links */}
-        <ul className="nav-links">
-          <li><Link href="/why">Why SkillForge</Link></li>
-<li><Link href="/projects">Projects</Link></li>
-          
-          {isClient && !loading && (
-            isLoggedIn ? (
-              <>
-                {/* --- THIS IS THE NEW SVG ICON LINK --- */}
-                <li>
-                  <Link href="/profile" className="profile-icon-link">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="profile-svg-icon"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </Link>
-                </li>
-                {/* --- END OF SVG ICON LINK --- */}
-                
-                <li><a onClick={handleLogout} className="btn btn-text logout-button">Logout</a></li>
-              </>
-            ) : (
-              <>
-                <li><Link href="/login" className="nav-action .nav-links .btn-secondary">Login</Link></li>
-                <li><Link href="/signup" className="nav-action .nav-links .btn-secondary">Signup</Link></li>
-              </>
-            )
-          )}
-        </ul>
-      </div>
-    </nav>
-    {/* Placeholder to prevent content jump under navbar */}
-    <div className="navbar-placeholder" aria-hidden="true"></div>
-  </>
+  return (
+    <>
+      <nav className={`navbar${scrolled ? ' scrolled' : ''}${navbarHidden ? ' navbar--hidden' : ''}`}>
+        <div className="container">
+          {/* The Logo */}
+          <Link href="/" className="logo">
+            <div style={{ width: '80px', height: '80px', position: 'relative' }}>
+              <Image
+                src="/logo.png"
+                alt="SkillFroge Logo"
+                fill
+                priority
+                style={{ objectFit: 'contain' }}
+              />
+            </div>
+          </Link>
+          {/* The Main Navigation Links */}
+          <ul className="nav-links">
+            <li><Link href="/why">Why SkillForge</Link></li>
+            <li><Link href="/projects">Projects</Link></li>
+            {isClient && !loading && (
+              isLoggedIn ? (
+                <>
+                  {/* --- THIS IS THE NEW SVG ICON LINK --- */}
+                  <li>
+                    <Link href="/profile" className="profile-icon-link">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="profile-svg-icon"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </Link>
+                  </li>
+                  {/* --- END OF SVG ICON LINK --- */}
+                  <li><a onClick={handleLogout} className="btn btn-text logout-button">Logout</a></li>
+                </>
+              ) : (
+                <>
+                  <li><Link href="/login" className="nav-action .nav-links .btn-secondary">Login</Link></li>
+                  <li><Link href="/signup" className="nav-action .nav-links .btn-secondary">Signup</Link></li>
+                </>
+              )
+            )}
+          </ul>
+        </div>
+      </nav>
+      {/* Placeholder to prevent content jump under navbar */}
+      <div className="navbar-placeholder" aria-hidden="true"></div>
+    </>
+  );
 }
