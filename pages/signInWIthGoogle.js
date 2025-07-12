392b25d2142f24ef94150cf21f4c926a040b4354
SkillForge <saranshsharma595@gmail.com>
2025-07-12 18:49:33 +0530

    Update signInWIthGoogle.js

--- pages/signInWIthGoogle.js
+++ pages/signInWIthGoogle.js
@@ -1,7 +1,7 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth, db } from "./firebase";
+import { GoogleAuthProvider, signInWithPopup } from "AuthContext.js";
+import { auth, db } from "config.js";
 import { toast } from "react-toastify";
-import { setDoc, doc } from "firebase/firestore";
+import { setDoc, doc } from "config.js";
 
 function SignInwithGoogle() {
   function googleLogin() {
