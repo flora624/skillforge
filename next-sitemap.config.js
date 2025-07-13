1469265231b0d0627422ae20f2cb506ebfe75f2d
SkillForge <saranshsharma595@gmail.com>
2025-07-13 17:06:21 +0530

    n

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://skillforgeprojects.vercel.app/', // TODO: Replace with your actual domain
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
