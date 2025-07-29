91eef3d233d7ab99c3f018a6475bc89a389b4d25
SkillForge <saranshsharma595@gmail.com>
2025-07-29 07:50:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    dynamicIO: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
 }
+
 
 module.exports = nextConfig
